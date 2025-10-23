98dca8c03d17c77a0d16a717557848667f6ecd10
JuanTrejo95 <jufratreve@gmail.com>
2025-10-23 11:06:16 -0600

    se quita url del feign client

--- usuario-service/src/main/java/com/usuario/service/feignclients/CarroFeignClient.java
+++ usuario-service/src/main/java/com/usuario/service/feignclients/CarroFeignClient.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import com.usuario.service.modelos.Carro;
 
-@FeignClient(name = "carro-service")
+@FeignClient(name = "carro-service", url = "http://localhost:8080", path = "/carro")
 public interface CarroFeignClient {
 
 	@PostMapping()
--- usuario-service/src/main/java/com/usuario/service/feignclients/MotoFeignClient.java
+++ usuario-service/src/main/java/com/usuario/service/feignclients/MotoFeignClient.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import com.usuario.service.modelos.Moto;
 
-@FeignClient(name = "moto-service")
+@FeignClient(name = "moto-service", url = "http://localhost:8080", path = "/moto")
 public interface MotoFeignClient {
 	
 	@PostMapping()
